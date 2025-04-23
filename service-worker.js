75f4c257fbd724caf1cc0ef0759acae0dec0d668
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2025-04-22 21:20:42 -0400

    mota curry

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,41 @@
 const CACHE_NAME = 'pet-master-cache-v1';
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/script.js',
-    '/resources/cat.png',
-    '/resources/stubborn.png',
-    '/resources/bowl.png',
-    '/resources/logo.png',
-    '/resources/thunderbolt.png',
-    '/resources/dropdown.png',
-    'https://kit.fontawesome.com/a076d05399.js',
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/manifest.json',
+  '/resources/cat.png',
+  '/resources/stubborn.png',
+  '/resources/bowl.png',
+  '/resources/thunderbolt.png'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((keyList) =>
-            Promise.all(
-                keyList.map((key) => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            )
-        )
-    );
-    self.clients.claim();
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
